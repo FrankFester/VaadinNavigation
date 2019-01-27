17e5acd52195fdda5510575486e69dec8b067b3e
FrankFester <ffester69@gmail.com>
2019-01-27 22:47:08 +0100

    UserService

--- src/main/java/de/kzvn/vaadinnavi/ui/view/UserService.java
+++ src/main/java/de/kzvn/vaadinnavi/ui/view/UserService.java
@@ -5,23 +5,99 @@
  */
 package de.kzvn.vaadinnavi.ui.view;
 
-import com.vaadin.flow.server.VaadinSession;
-import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
-import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Frank
  */
-
+@Service
 public class UserService {
 
-    public HashMap<String, String> getUserListe() {
-        HashMap<String, String> userMap = new HashMap<>();
-       // Collection<VaadinSession> sessions = VaadinSession.getAllSessions();
+    private HashMap<String, User> userMap = new HashMap<>();
 
-        return userMap;
+    public class User {
+        String userName;
+        Date loginDate;
+        Date logOffDate;
+        boolean loggedIn;
+
+        public User(String userName, Date loginDate, Date logOffDate, boolean loggedIn) {
+            this.userName = userName;
+            this.loginDate = loginDate;
+            this.logOffDate = logOffDate;
+            this.loggedIn = loggedIn;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public Date getLoginDate() {
+            return loginDate;
+        }
+
+        public void setLoginDate(Date loginDate) {
+            this.loginDate = loginDate;
+        }
+
+        public Date getLogOffDate() {
+            return logOffDate;
+        }
+
+        public void setLogOffDate(Date logOffDate) {
+            this.logOffDate = logOffDate;
+        }
+
+        public boolean isLoggedIn() {
+            return loggedIn;
+        }
+
+        public void setLoggedIn(boolean loggedIn) {
+            this.loggedIn = loggedIn;
+        }
+             
+    }   
+    
+      
+    public boolean addUserLoggedIn(String userName) {
+        User u = userMap.get(userName);
+        if ((u != null) && (u.isLoggedIn())) {
+                // Benutzer bereits angemeldet...
+                // Logik, ob 2. Mal möglich???
+                // LOG.info("User {} hat sich bereits am {} angemeldet...",u.getUserName(), u.getLoginDate());
+                return false;
+                ///
+        } else if (u!=null) {
+                u.setLoginDate(new Date());
+                u.setLoggedIn(true);
+        } else {
+            u = new User(userName,new Date(),null,true);
+        }
+        userMap.put(userName, u);
+        return true;
+
+   }
+    
+    public void removeUserLogff(String userName) {
+        User u = userMap.get(userName);
+        if(u != null) {
+            u.setLogOffDate(new Date());
+            u.setLoggedIn(false);
+            userMap.put(userName, u);
+        } else {
+            // Fehler...
+        }
+   }   
+    
+    public HashMap<String, User> getUserMap() {
+        return this.userMap;   
     }
 
 }
